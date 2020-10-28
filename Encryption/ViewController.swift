ddaf9f8c228be8f6d3c15b43f0bbf00a2b77924f
gurugovindaprasad <gurugovindaprasad@gmail.com>
2020-10-28 11:52:28 +0530

    Initial Commit

---
+++ Encryption/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Encryption
+//
+//  Created by Guru Ranganathan on 10/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
